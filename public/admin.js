8a31f1167a1f36f067172ffba43b4b29f19c8ef4
Austin bambino <link9215@gmail.com>
2022-02-19 21:23:39 -0500

    tried my best

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,33 @@
+async function main() {
+    let response = await fetch ('http://localhost:3001/listBooks')
+    let books =await response.json()
+    books.forEach(renderBook)
+}
+function renderBook(book) {
+    let root = document.querySelector('#root')
+    let li = document.createElement('li')
+    li.textContent = book.title
 
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let buttonSave = document.createElement('button')
+    buttonSave.textcontent = 'save this'
+
+    buttonSave.addEventListener('click', function(){
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+    li.append(quantityInput, buttonSave)
+    root.append(li)
+}
+main();
 // Your Code Here
